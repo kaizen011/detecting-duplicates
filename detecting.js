b683e04d504071aafa1180b2c1b03a824d523e22
kaizen011 <kaizen011@users.noreply.github.com>
2024-02-11 15:03:19 +0100

    Whitespace clean

--- detecting.js
+++ detecting.js
@@ -6,11 +6,9 @@ function removeDuplicates(nums) {
             uniqueNums.push(num);
         }
     }
-
     return uniqueNums;
 }
 
-
-const numbers = [1, 2, 3, 4, 5, 2, 6, 7, 3,3,2,1,5,3,12,4,1,2,54,3,2,2,2];
+const numbers = [1,2,3,4,5,2,6,7,3,3,2,1,5,3,12,4,1,2,54,3,2,2,2];
 const cleanedArray = removeDuplicates(numbers);
 console.log("Cleaned array:", cleanedArray); 
